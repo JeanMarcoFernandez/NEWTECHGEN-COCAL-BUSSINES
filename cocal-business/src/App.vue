4dfe5ced5dac9d5268e94e0d4bfb1fbbd182224a
AlinaMollinedo <alina.mollinedo@ucb.edu.bo>
2025-10-24 01:13:28 -0400

    Landing Page (Boceto)

---
+++ cocal-business/src/App.vue
@@ -0,0 +1,124 @@
+<script setup>
+import { RouterView, RouterLink, useRoute } from 'vue-router'
+import { ref } from 'vue'
+
+const drawer = ref(false)
+
+</script>
+
+<template>
+
+<v-app>
+  <v-app-bar class="pa-6" flat fully-hide>
+    <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-row class="hidden-sm-and-down align-center pa-4 app-bar">
+      <v-col cols="1" class="justify-start">
+        <router-link to="/">
+          <v-img src="/assets/ntg_logo.jpg" max-width="63" class="rounded-lg"/> 
+        </router-link>
+      </v-col>
+      <v-col cols="11" class="d-flex justify-end">
+        <v-btn
+          class="app-bar-login-btn"
+          size="x-large"
+        >Inicar Sesión</v-btn>
+        <v-btn
+          class="app-bar-register-btn"
+          size="x-large"
+        >Regístrate</v-btn>
+      </v-col>
+    </v-row>
+  </v-app-bar>
+
+  <v-navigation-drawer v-model="drawer" class="hidden-md-and-up">
+    <v-list>
+      <v-list-item to="/">
+        <v-list-item-title>Inicio</v-list-item-title>
+      </v-list-item>
+      <v-list-item>
+        Iniciar Sesión
+      </v-list-item>
+      <v-list-item>
+        Regístrate
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+
+  <v-main >
+    <v-container>
+      <RouterView/>
+    </v-container>
+  </v-main>
+
+  <v-footer>
+    <v-row>
+      <v-col cols="12">
+        <v-row>
+          <v-col cols="12" sm="4">
+            <v-img src="/assets/ntg_logo.jpg" max-width="60" class="rounded-lg"/>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <p class="footer-title">Síguenos en nuestras redes sociales</p>
+            <v-row>
+              <v-btn icon="mdi-facebook"/>
+              <v-btn icon="mdi-instagram"/>
+              <v-btn icon="mdi-twitter"/>
+            </v-row>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <p class="footer-title">Contáctanos</p>
+            <p class="footer-text">
+              Av. 14 de septiembre, Calle 2
+            </p>
+            <p class="footer-text">
+              +591 7005060
+            </p>
+            <p class="footer-text">
+              support@ntg.com
+            </p>
+          </v-col>
+        </v-row>
+        <v-row class="d-flex flex-row justify-end">
+          <p>&copy New Tech Gen</p>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-footer>
+</v-app>
+
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Slabo+27px&family=Tinos:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Red+Hat+Text:ital,wght@0,300..700;1,300..700&family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Slabo+27px&family=Tinos:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Funnel+Display:wght@300..800&family=Red+Hat+Text:ital,wght@0,300..700;1,300..700&family=Roboto+Flex:opsz,wght@8..144,100..1000&family=Slabo+27px&family=Tinos:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+
+.v-app-bar{
+  background-color: transparent !important;
+}
+
+.v-main {
+  padding-top: 30px;
+  background: linear-gradient(50deg, #778fd9, #bbc8f0);
+}
+
+.v-footer{
+  max-height: 500px;
+  background-color: #324375 !important;
+}
+
+.footer-title{
+  font-family: 'Tinos', serif;
+  color: white;
+  font-weight: bold;
+  font-size: large;
+}
+
+.footer-text{
+  font-family: 'Red Hat Text', sans-serif;
+  color: white;
+  font-size: medium;
+}
+
+</style>
---
+++ cocal-business/src/components/LandingPage.vue
@@ -0,0 +1,176 @@
+<template>
+<v-row class="d-flex flex-row align-center">
+    <v-col cols="12" sm="7">
+        <h1>Sincroniza tu equipo en tiempo real, sin estrés.</h1>
+        <p>
+            No importa si eres una startup o una gran corporación, 
+            CoCal Business te ofrece una solución ágil y potente 
+            para gestionar calendarios, reuniones y proyectos de forma colaborativa, 
+            ahorrando tiempo y aumentando la eficiencia de tu equipo.
+        </p>
+        <v-row>
+            <v-col cols="3">
+                <v-btn>Empezar</v-btn>
+            </v-col>
+            <v-col cols="6">
+                <p>
+                    ¿Estás listo para transformar tus reuniones? Comienza ahora con CoCal Business
+                </p>
+            </v-col>
+        </v-row>
+    </v-col>
+    <v-col cols="12" sm="5">
+        <v-img src="/assets/cocalbusiness_logo_transparent.png" max-height="350"/>
+    </v-col>
+</v-row>
+
+<v-row>
+    <v-col cols="12" sm="4" class="text-center">
+        <v-icon size="x-large">mdi-account-group-outline</v-icon>
+        <v-card flat>
+            <v-card-title>Conecta a tu equipo en un solo clic</v-card-title>
+            <v-card-text>
+                Gestiona equipos de forma sencilla, integrando múltiples calendarios y 
+                permitiendo ver los horarios disponibles de todos al instante. Coordina reuniones
+                 y proyectos sin las típicas confusiones de agendar.
+            </v-card-text>
+        </v-card>
+    </v-col>
+    <v-col cols="12" sm="4" class="text-center">
+        <v-icon size="x-large">mdi-calendar-check</v-icon>
+        <v-card flat>
+            <v-card-title>Calendarios sincronizados, reuniones sin estrés</v-card-title>
+            <v-card-text>
+                No más malentendidos a la hora de fijar reuniones. Con CoCal Business, puedes combinar 
+                los calendarios de tu equipo y ver los espacios libres de todos en tiempo real, asegurando 
+                que nunca más se solapen horarios.
+            </v-card-text>
+        </v-card>
+    </v-col>
+    <v-col cols="12" sm="4" class="text-center">
+        <v-icon size="x-large">mdi-projector</v-icon>
+        <v-card flat>
+            <v-card-title>Optimiza recursos en tiempo real</v-card-title>
+            <v-card-text>
+                ¿Necesitas una sala de reuniones o equipo específico para un evento? 
+                CoCal Business te permite gestionar y reservar recursos como salas de reuniones, 
+                proyectores, y otros equipos esenciales, todo desde la misma plataforma.
+            </v-card-text>
+        </v-card>
+    </v-col>
+</v-row>
+
+<v-row class="d-flex flex-row align-center">
+    <v-col cols="12" sm="5">
+        <v-img src="/assets/calendar_illus.jpg" max-width="600"/>
+    </v-col>
+    <v-col cols="12" sm="7">
+        <h1>Visibilidad total sobre calendarios y recursos de tu equipo</h1>
+        <p>
+            Con CoCal Business, tendrás acceso a la disponibilidad de todos los
+             miembros de tu equipo, así como a la gestión de recursos como salas de reuniones y equipos. 
+             Podrás ver los espacios libres y coordinar reuniones sin preocupaciones, optimizando el tiempo de todos.
+        </p>
+    </v-col>
+</v-row>
+
+<v-row class="d-flex flex-row align-center">
+    <v-col cols="12" sm="7">
+        <h1>Lleva tu Productividad al Siguiente Nivel con la App Móvil</h1>
+        <p>
+            Con la app móvil de CoCal Business, puedes revisar el resumen de cada reunión después de que haya terminado 
+            y proporcionar comentarios al instante. Nunca más olvidarás detalles importantes o retrasos, y podrás dar 
+            retroalimentación en tiempo real, mejorando la comunicación y la colaboración.
+        </p>
+    </v-col>
+    <v-col cols="12" sm="5">
+        <v-img src="/assets/cocal_mobile.png" max-height="400"/>
+    </v-col>
+</v-row>
+
+<v-divider/>
+
+<v-row>
+    <v-col cols="12" class="text-center">
+    <h1>Acerca de nosotros</h1>
+    <p>Somos un equipo de visionarios, diseñadores e ingenieros dedicados a crear soluciones tecnológicas que optimicen 
+        la forma en que las empresas interactúan, planifican y ejecutan sus proyectos. Creemos que la tecnología debe ser 
+        un aliado que impulse el éxito, no una barrera que lo limite.</p>
+    </v-col> 
+</v-row>
+<v-row>
+    <v-col cols="12" sm="6">
+        <h2>Visión</h2>
+        <p>En New Gen Tech, visualizamos un futuro donde la tecnología simplifique 
+            la vida empresarial, creando herramientas intuitivas que faciliten la 
+            colaboración, la gestión del tiempo y el crecimiento de las empresas, 
+            independientemente de su tamaño.</p>
+    </v-col>
+    <v-col cols="12" sm="6">
+        <h2>Misión</h2>
+        <p>Nuestra misión es transformar la manera en que las empresas organizan sus equipos y 
+            recursos, ofreciendo soluciones innovadoras como CoCal Business. Buscamos mejorar la 
+            productividad, la comunicación y la eficiencia a través de tecnología avanzada, 
+            creando plataformas que faciliten el trabajo colaborativo y ayuden a las organizaciones 
+            a alcanzar su máximo potencial.</p>
+    </v-col>
+</v-row>
+
+<v-row class="justify-center">
+    <h2>Nuestros valores</h2>
+</v-row>
+<v-row>
+    <v-col cols="6" class="text-center">
+        <v-icon>mdi-lightbulb-on-20</v-icon>
+        <v-card flat>
+            <v-card-title>Innovación constante</v-card-title>
+            <v-card-text>Buscamos siempre nuevas formas de mejorar y adaptarnos a los cambios del mercado.</v-card-text>
+        </v-card>
+    </v-col>
+    <v-col cols="6" class="text-center">
+        <v-icon>mdi-account-heart</v-icon>
+        <v-card flat>
+            <v-card-title>Colaboración abierta</v-card-title>
+            <v-card-text>Fomentamos un entorno de trabajo colaborativo, donde cada idea es bienvenida y se valora.</v-card-text>
+        </v-card>
+    </v-col>
+</v-row>
+<v-row>
+    <v-col cols="12" class="text-center">
+        <v-icon>mdi-flare</v-icon>
+        <v-card flat>
+            <v-card-title>Simplicidad y eficiencia</v-card-title>
+            <v-card-text>Creemos en herramientas que sean poderosas, pero fáciles de usar.</v-card-text>
+        </v-card>
+    </v-col>
+</v-row>
+
+</template>
+
+
+<style scoped>
+
+h1{
+    font-family: 'Tinos', serif;
+    font-size: xx-large;
+}
+
+h2{
+    font-family: 'Tinos', serif;
+    font-size: x-large;
+}
+
+p, .v-card-text{
+    font-family: 'Roboto Flex', sans-serif;
+    font-size: large;
+}
+
+.v-card-title{
+    font-family: 'Funnel Display', sans-serif;
+    font-size: larger;
+}
+
+.v-card{
+    background-color: transparent;
+}
+</style>
