89399bc078c946c8f0aff1e5cfacf9a20ca23e8b
Raquel Osorio <142281150+Osorio-Raquel@users.noreply.github.com>
2025-11-26 22:03:35 -0400

    Delete Backend-Cocal directory

--- Backend-Cocal/testEmail.js
+++
@@ -1,9 +0,0 @@
-import { sendEmail } from "./services/emailService.js";
-
-(async () => {
-  await sendEmail({
-    to: "raquel.osorio@ucb.edu.bo",
-    subject: "Prueba de COCAL Backend",
-    html: "<h2>Hola desde el backend 👋</h2><p>Si ves esto, todo funciona.</p>",
-  });
-})();
